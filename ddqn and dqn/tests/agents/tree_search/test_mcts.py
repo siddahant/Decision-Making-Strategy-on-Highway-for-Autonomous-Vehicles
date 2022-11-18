e21f1b6d8759b85ef5feadac1c48dd7212b8caeb
Siddahant Jain <44742647+siddahant@users.noreply.github.com>
2022-11-17 17:28:49 -0700

    Progress Check 2

---
+++ ddqn and dqn/tests/agents/tree_search/test_mcts.py
@@ -0,0 +1,19 @@
+import gym
+from rl_agents.agents.tree_search.mcts import MCTSAgent
+
+
+def test_cartpole():
+    env = gym.make('CartPole-v0')
+    agent = MCTSAgent(env, config=dict(budget=400, temperature=200, max_depth=10))
+
+    state, info = env.reset()
+    done = truncated = False
+    steps = 0
+    while not done and not truncated:
+        action = agent.act(state)
+        assert action is not None
+
+        next_state, reward, done, truncated, info = env.step(action)
+        steps += 1
+
+    assert steps == env._max_episode_steps
---
+++ ddqn and dqn/tests/trainer/test_evaluation.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import gym
+
+from rl_agents.agents.simple.random import RandomUniformAgent
+from rl_agents.trainer.evaluation import Evaluation
+
+
+def test_evaluation(tmpdir):
+    env = gym.make('CartPole-v0')
+    agent = RandomUniformAgent(env)
+    evaluation = Evaluation(env,
+                            agent,
+                            directory=tmpdir,
+                            num_episodes=3,
+                            display_env=False,
+                            display_agent=False,
+                            display_rewards=False)
+    evaluation.train()
+    assert any(['metadata' in file.name for file in evaluation.run_directory.iterdir()])
